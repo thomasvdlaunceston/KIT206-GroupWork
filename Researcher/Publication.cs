61a3c1ebeb2a352051e03d133fd5e3cb43a0c4c7
Thomas <thomasvd@utas.edu.au>
2021-05-04 16:48:30 +1000

    Merging the Repository

---
+++ Researcher/Publication.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+public class Publication
+{
+	public Publication()
+	{
+	}
+}
---
+++ Researcher/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_2_Console_application_
+{
+    class Student
+    {
+        string Degree;
+        string supervisior;
+    }
+}
