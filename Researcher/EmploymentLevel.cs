0330c79ad21c6549336600a4d86c5638c582a74d
Thomas <thomasvd@utas.edu.au>
2021-05-04 17:18:15 +1000

    Position and Employment Level Draft 1 Finished

---
+++ Researcher/EmploymentLevel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KIT206_GroupWork.Researcher
+{
+    public enum EmploymentLevel
+    {
+        Student,
+        A,
+        B,
+        C,
+        D,
+        E
+    }
+}
--- Researcher/Position.cs
+++ Researcher/Position.cs
@@ -1,8 +1,49 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class Position
+namespace KIT206_GroupWork.Researcher
 {
-	public Position()
+	public class Position
 	{
+		EmploymentLevel level;
+		DateTime start;
+		DateTime end;
+		Dictionary<EmploymentLevel, string> convertTitle = new Dictionary<EmploymentLevel, string>();
+
+		public Position()
+		{
+			convertTitle[EmploymentLevel.Student] = "Student";
+			convertTitle[EmploymentLevel.A] = "Postdoc";
+			convertTitle[EmploymentLevel.B] = "Lecturer";
+			convertTitle[EmploymentLevel.C] = "Senior Lecturer";
+			convertTitle[EmploymentLevel.D] = "Associate Professor";
+			convertTitle[EmploymentLevel.E] = "Professor";
+		}
+
+		public string title()
+		{
+			return convertTitle[level];
+			/*switch (level)
+			{
+				case EmploymentLevel.Student:
+					return "Student";
+				case EmploymentLevel.A:
+					return "Postdoc";
+				case EmploymentLevel.B:
+					return "Lecturer";
+				case EmploymentLevel.C:
+					return "Senior Lecturer";
+			}*/
+		}
+
+		public string ToTitle(EmploymentLevel I)
+        {
+			return convertTitle[I];
+        }
+
 	}
+
 }
