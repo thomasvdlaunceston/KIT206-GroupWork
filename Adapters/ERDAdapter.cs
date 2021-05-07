45db49c947d7be2c751fbc718430492f8ece3e8c
Thomas <thomasvd@utas.edu.au>
2021-05-07 22:30:37 +1000

    Working on additional work for Tuesday that is I realise requried

--- Adapters/ERDAdapter.cs
+++ Adapters/ERDAdapter.cs
@@ -78,7 +78,127 @@ namespace KIT206_GroupWork.Adapters
             return researcherList.ToArray();
 
         }
-        /*public static Researcher.Researcher fetchFullResearcherDetails(int id) { }
+        public static Researcher.Researcher fetchFullResearcherDetails(int id) 
+        {
+            MySqlDataReader rdr = null;
+            List<Researcher.Position> positions = new List<Researcher.Position>();
+            GetConnection();
+
+            try
+            {
+                // Open the connection
+                conn.Open();
+
+                // 1. Instantiate a new command with a query and connection
+                MySqlCommand cmd = new MySqlCommand("select level, start, end from researcher where id = " + id, conn);
+
+                // 2. Call Execute reader to get query results
+                rdr = cmd.ExecuteReader();
+
+                // print the CategoryName of each record
+                while (rdr.Read())
+                {
+                    //This illustrates how the raw data can be obtained using an indexer [] or a particular data type can be obtained using a GetTYPENAME() method.
+                    //Console.WriteLine("{0} {1}", rdr[0], rdr.GetString(1));
+                    Researcher.Researcher res = new Researcher.Researcher { GivenName = rdr.GetString(1), FamilyName = rdr.GetString(2), ID = rdr.GetInt32(0), Title = rdr.GetString(4) };
+                    //https://stackoverflow.com/questions/20547261/database-field-enum-to-c-sharp-list
+                    var enumerated = rdr[3] != DBNull.Value ? rdr.GetString(3) : "Student";
+                    Researcher.Position pos = new Researcher.Position { level = (Researcher.EmploymentLevel)Enum.Parse(typeof(Researcher.EmploymentLevel), enumerated) };
+                    res.positions.Add(pos);
+                    //Employee e = new Employee { Name = combined, ID = rdr.GetInt32(2) };
+                    researcherList.Add(res);
+                }
+            }
+            finally
+            {
+                // close the reader
+                if (rdr != null)
+                {
+                    rdr.Close();
+                }
+
+                // Close the connection
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }
+            return researcherList.ToArray();
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            try
+            {
+                // Open the connection
+                conn.Open();
+
+                // 1. Instantiate a new command with a query and connection
+                MySqlCommand cmd = new MySqlCommand("select type,given_name,family_name,title,unit,campus,email,photo,degree,supervisor_id,level,utas_start,current_start from researcher where id = "+id, conn);
+
+                // 2. Call Execute reader to get query results
+                rdr = cmd.ExecuteReader();
+
+                // print the CategoryName of each record
+                while (rdr.Read())
+                {
+                    //This illustrates how the raw data can be obtained using an indexer [] or a particular data type can be obtained using a GetTYPENAME() method.
+                    //Console.WriteLine("{0} {1}", rdr[0], rdr.GetString(1));
+                    Researcher.Researcher res = new Researcher.Researcher { GivenName = rdr.GetString(1), FamilyName = rdr.GetString(2), ID = rdr.GetInt32(0), Title = rdr.GetString(4) };
+                    //https://stackoverflow.com/questions/20547261/database-field-enum-to-c-sharp-list
+                    var enumerated = rdr[3] != DBNull.Value ? rdr.GetString(3) : "Student";
+                    Researcher.Position pos = new Researcher.Position { level = (Researcher.EmploymentLevel)Enum.Parse(typeof(Researcher.EmploymentLevel), enumerated) };
+                    res.positions.Add(pos);
+                    //Employee e = new Employee { Name = combined, ID = rdr.GetInt32(2) };
+                    researcherList.Add(res);
+                }
+            }
+            finally
+            {
+                // close the reader
+                if (rdr != null)
+                {
+                    rdr.Close();
+                }
+
+                // Close the connection
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }
+            return researcherList.ToArray();
+
+        }
+    }
+        /*
         public static Researcher.Researcher completeResearcherDetails(Researcher.Researcher r) { }
         public static Researcher.Publication[] fetchBasicPublicationDetails(Researcher.Researcher r) { }
         public static Researcher.Publication  completePublicationDetails(Researcher.Publication p) { }
